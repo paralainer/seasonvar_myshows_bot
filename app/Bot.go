e50bea979387f8fd2a36f8f5d544facfa77aed5a
paralainer <serg.talov@gmail.com>
2017-11-03 23:04:54 +1100

    Add more easy search

--- app/Bot.go
+++ app/Bot.go
@@ -14,7 +14,7 @@ var MyShowsUnseenRegexp = regexp.MustCompile(`(.*) /show_\d+\n.*\ns(\d+)e(\d+).*
 var MyShowsNewRegexp = regexp.MustCompile(`Новый эпизод сериала (.*)\n.*s(\d+)e(\d+).*`)
 var MyShowsLinkRegexp = regexp.MustCompile(`https?://myshows\.me/view/episode/(\d+)/?`)
 var SearchRegexp = regexp.MustCompile(`(.*):\s*(\d+)\s*:\s*(\d+)\s*`)
-var SearchSpacesRegexp = regexp.MustCompile(`(.*)\s+(\d+)\s+(\d+).*`)
+var SearchSpacesRegexp = regexp.MustCompile(`(.*)\s+(\d+)\s+(\d+)\s*`)
 
 type TgBot struct {
 	Api       *tgbotapi.BotAPI
